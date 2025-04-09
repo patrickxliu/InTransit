a0b41e30df1990d7cca558082955e364ef72fa52
pxliu <patrickxinyiliu@gmail.com>
2025-04-09 12:30:04 -0400

    visualize rest

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-
+<h1>TOD Scores of MBTA Communities</h1>
+<h2>Transit</h2>
 <iframe title="Hello" src="transit.html" width="100%" height="600px" />
+<h2>Orientation</h2>
+<iframe title="Hello" src="orientation.html" width="100%" height="600px" />
+<h2>Development</h2>
+<iframe title="Hello" src="development.html" width="100%" height="600px" />
+
+
+<style>
+    h1{
+        font-family: helvetica;
+    }
+    h2{
+        font-family: helvetica;
+    }
+</style>
