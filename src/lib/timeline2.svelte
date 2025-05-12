b495d386ac2a9c3c8e0c3a2485d0c8a079208e6c
Emily Gan <emily.gan496@gmail.com>
2025-05-12 11:46:06 -0400

    train

--- src/lib/timeline2.svelte
+++ src/lib/timeline2.svelte
@@ -268,7 +268,7 @@
       <!-- Train -->
       <img
         class="train"
-        src="/train.TIFF"
+        src="/train2.png"
         alt="Train"
         style="--trainX: {scrollProgress * trainMaxMove}px"
       />
